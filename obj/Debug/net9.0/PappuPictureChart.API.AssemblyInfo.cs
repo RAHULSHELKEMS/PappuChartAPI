3026ca842ad6bfcff64da66f4fdc628e5ddd4efe
Rahul <rahulshelkems@gmail.com>
2026-06-22 22:04:05 +0530

    Added Docker files

--- obj/Debug/net9.0/PappuPictureChart.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/PappuPictureChart.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PappuPictureChart.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d514caafb229e03e9513391ab28dabffb1629fef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4461764fd4e9f93db8fe99bdc0db78baf3abd4b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PappuPictureChart.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PappuPictureChart.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
